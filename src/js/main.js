aea6c635300ee373a775f47c2330b9547cfbc2eb
Max Zhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2019-07-14 01:24:55 +0300

    Add an on-page log area

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 // Grab elements, create settings, etc.
 var videos = document.getElementsByClassName('video');
+var logArea = document.getElementById('log');
 
 // Get access to the camera!
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -7,9 +8,14 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
         //video.src = window.URL.createObjectURL(stream);
 //        console.log(stream);
+//        log(stream);
         for (let video of videos) {
             video.srcObject = stream;
             video.play();
         };
     });
+}
+
+function log(obj) {
+    logArea.value = logArea.value + JSON.stringify(obj) + "\n";
 }
