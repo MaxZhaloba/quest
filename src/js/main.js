b92c33b4a7bbcae8a4b2359f8fec00df68807c53
Max Zhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2019-07-14 13:04:19 +0300

    Get media tracks

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,30 @@
 var videos = document.getElementsByClassName('video');
 var logArea = document.getElementById('log');
 
+log("start");
+
+log(navigator.mediaDevices.getUserMedia);
+log("continue");
+
 // Get access to the camera!
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
         //video.src = window.URL.createObjectURL(stream);
 //        console.log(stream);
-//        log(stream);
+        log(JSON.stringify(stream));
+        log("called");
+//        log(stream.getVideoTracks());
+        var tracks = stream.getVideoTracks();
+        log("tacks: " + tracks.length);
         for (let video of videos) {
+//            console.log(stream);
             video.srcObject = stream;
             video.play();
         };
     });
 }
 
-function log(obj) {
-    logArea.value = logArea.value + JSON.stringify(obj) + "\n";
+function log(string) {
+    logArea.value = logArea.value + string + "\n";
 }
