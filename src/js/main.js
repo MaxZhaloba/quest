bf6bebd413ed58ad09b8942702039f2fd5ed2276
Max Zhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2019-07-16 01:50:09 +0300

    Cleanup the JS code

--- src/js/main.js
+++ src/js/main.js
@@ -5,30 +5,19 @@ var playButton = document.getElementById('play');
 
 log("start");
 
-log(navigator.mediaDevices.getUserMedia);
-log("continue");
-
 // Get access to the camera!
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-        //video.src = window.URL.createObjectURL(stream);
-//        console.log(stream);
-        log(JSON.stringify(stream));
-        log("called");
-//        log(stream.getVideoTracks());
-        var tracks = stream.getVideoTracks();
-        log("tacks: " + tracks.length);
+        log("getUserMedia callback");
 
         playButton.addEventListener('click', function() {
             for (let video of videos) {
-    //            console.log(stream);
                 video.srcObject = stream;
                 log(video.play);
                 video.play();
             };
         });
-
     }).catch(function (error) {
         log("Something went wrong!");
         log(error);
