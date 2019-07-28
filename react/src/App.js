671e1a93e94d078f2d7aa0bf195dfb06a4f2d5ca
Max Zhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2019-07-28 23:41:16 +0300

    Render a simplistic 3D scene with A-Frame and React

--- react/src/App.js
+++ react/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
+import 'aframe';
+import {Entity, Scene} from 'aframe-react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>3D content</h1>
+        <a-scene>
+            <a-box color="teal" position="5 5 -20" rotation="0 45 45"/>
+        </a-scene>
     </div>
   );
 }
