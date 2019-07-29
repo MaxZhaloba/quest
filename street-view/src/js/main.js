7d1c7743eb503bd3beeb6bb8685f7aa91d634aaf
Max Zhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2019-07-29 18:41:05 +0300

    Implement basic moving across Google's panorama places

---
+++ street-view/src/js/main.js
@@ -0,0 +1,38 @@
+    var panorama = null;
+    var currentPlaceIndex = 0;
+    const places = [
+        { panId: 'tjW_9p-_OzKrYxPRjmvyrQ', heading: 15, pitch: 0},
+        { panId: 'CSKy2bwnay5X3alzAHEPFw', heading: 15, pitch: 0},
+        { panId: 'CSKy2bwnay5X3alzAHEPFw', heading: -15, pitch: 15},
+        { panId: 'CSKy2bwnay5X3alzAHEPFw', heading: -15, pitch: 0},
+        { panId: 'r3d-ZzK2WMmJ0JXorfP4ng', heading: -30, pitch: 0},
+        { panId: 'r3d-ZzK2WMmJ0JXorfP4ng', heading: -45, pitch: 0},
+        { panId: 'r3d-ZzK2WMmJ0JXorfP4ng', heading: -60, pitch: 0},
+        { panId: 'r3d-ZzK2WMmJ0JXorfP4ng', heading: -75, pitch: 0}
+    ];
+
+      function initialize() {
+        panorama = new google.maps.StreetViewPanorama(
+            document.getElementById('pano'), {
+              pov: {
+                heading: 15,
+                pitch: -5
+              }
+            });
+        moveToCurrentPlace();
+      }
+
+      function moveToCurrentPlace() {
+        var currentPlace = places[currentPlaceIndex];
+
+        panorama.setPano(currentPlace.panId);
+        panorama.setPov({ heading: currentPlace.heading, pitch: currentPlace.pitch });
+      }
+
+    document.getElementById("move").addEventListener("click", function(){
+        currentPlaceIndex += 1;
+        if (currentPlaceIndex >= places.length)
+            currentPlaceIndex = 0;
+
+        moveToCurrentPlace();
+    });
